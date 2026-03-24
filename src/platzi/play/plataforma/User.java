a19b7a3473f16374a8321b37022c45b7d7caf7b5
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-03-24 17:21:46 -0500

    [+] User.java: Encapsula atributos de clase como 'private' e Implementa Getters/Setters

--- src/platzi/play/plataforma/User.java
+++ src/platzi/play/plataforma/User.java
@@ -6,10 +6,10 @@ import java.time.LocalDateTime;
 
 public class User {
     // Atributos
-    public String name;
-    public String email;
-    public String password;
-    public LocalDateTime createdAt;
+    private String name;
+    private String email;
+    private String password;
+    private final LocalDateTime createdAt;      // Es constante no cambia, por eso usamos final
 
     // Constructor
     public User( String name, String email ) {
@@ -28,5 +28,34 @@ public class User {
                 "\n Email: " + email +
                 "\n Password: " + password +
                 "\n Fecha de creación: " + createdAt.toString();
+    }
+
+    // Getters & Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
